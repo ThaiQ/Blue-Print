16b1dd2412ea0b33953f46d12c88be6a6051716f
ThaiQ <quachhongthai200@gmail.com>
2019-04-28 21:37:34 -0700

    ssudoku

--- sudoku/SudoFunctions.js
+++ sudoku/SudoFunctions.js
@@ -7,38 +7,61 @@ constructor(board){
 //Print out game board
 print()
 {
+	console.log("    1  2  3  4  5  6  7  8  9");
+	console.log("    -------------------------");
+	var char = "a";
 	for(var i = 0 ; i<this.board.length; i++)
 	{
-		var line ="";
+		var line = char + " | ";
         	for (var j = 0 ; j<this.board[i].length; j++)
         	{
-                	line = line + this.board[i][j] + " ";
+                	line = line + this.board[i][j] + "  ";
         	}
-        	console.log(line)
+        	console.log(line);
+		char = this.nextChar(char);
 	}
+}
+
+//increment letter, uses for print
+nextChar(c) {
+    return String.fromCharCode(c.charCodeAt(0) + 1);
 }
 
 //Check for validation
 check(){
 
 //Row
-for (var i = 0 ; i < this.board.length; i++){
-var line=[];
-	for (var j = 0 ; j<this.board.length; j++){
+for (let i = 0 ; i < this.board.length; i++){
+let line=[];
+	for (let j = 0 ; j<this.board.length; j++){
 		line.push(this.board[i][j])}
-	console.log(line);
 	if (!this.validLine(line))
 		return false;
 }
 
 //Column
-for (var i = 0 ; i < this.board.length; i++){
-var line=[];
-        for (var j = 0 ; j<this.board.length; j++){
+for (let i = 0 ; i < this.board.length; i++){
+let line=[];
+        for (let j = 0 ; j<this.board.length; j++){
                 line.push(this.board[j][i])}
-        console.log(line);
         if (!this.validLine(line))
                 return false;
+}
+
+//3x3 box
+var incrementRow = 0;
+var incrementCol = 0;
+for (let box = 0; box < 9 ; box ++){
+let line =[];
+	for (let col = 0 + incrementCol; col<3 + incrementCol; col ++){
+		for (let row = 0 + incrementRow; row<3 + incrementRow; row ++){
+			line.push(this.board[col][row]);
+			if (!this.validLine(line))
+							return false;
+		}
+	}
+	 if(incrementRow<=9-1-3){incrementRow += 3;}
+	 else {incrementRow = 0; incrementCol += 3;}
 }
 
 return true;
@@ -46,12 +69,45 @@ return true;
 
 //Check for Zeros="." and Repeat
 validLine(line){
-	for (var i=0; i<line.length; i++){
-	for (var j=i+1; j<line.length; j++){
+	for (let i=0; i<line.length; i++){
+	for (let j=i+1; j<line.length; j++){
 		if(line[i]<1 || line[i]>9 || line[i]=="." || line[i]==line[j] || !Number.isInteger(line[i])){return false;}
 	}}
 	return true;
 }
+
+//assigns each 9 letters to 9 numbers
+letterToNumber(letter){
+	if (letter == "a")return 0;
+	else if (letter == "b")return 1;
+	else if (letter == "c")return 2;
+	else if (letter == "d")return 3;
+	else if (letter == "e")return 4;
+	else if (letter == "f")return 5;
+	else if (letter == "g")return 6;
+	else if (letter == "h")return 7;
+	else if (letter == "i")return 8;
+	else return "Must be from a to i";
+}
+
+	inputBoard(input){
+		let array = input.split("");
+		if(array.length==4 && input.charAt(2)=='-'){
+			let char1 = parseInt(array[0]);
+			let char2 = this.letterToNumber(array[1]);
+			let char4 = parseInt(array[3]);
+			if(Number.isInteger(char1) &&  Number.isInteger(char2) && Number.isInteger(char4)){
+				this.board[char2][char1-1] = char4;
+				return true;
+			}
+		}
+		return false;
+	}
+
+	clear(){
+		console.log("clean")
+		this.board=this.board_Origin;
+	}
 
 }
 
--- sudoku/sudo.js
+++ sudoku/sudo.js
@@ -1,7 +1,8 @@
-var methodsFile = require('./SudoFunctions.js')
-console.log("start");
+var methodsFile = require('./SudoFunctions.js');
+var input = require('readline-sync');;
 
-var board = [
+//Boards
+var board_Empty = [
 [".",".",".",".",".",".",".",".","."],
 [".",".",".",".",".",".",".",".","."],
 [".",".",".",".",".",".",".",".","."],
@@ -12,7 +13,18 @@ var board = [
 [".",".",".",".",".",".",".",".","."],
 [".",".",".",".",".",".",".",".","."]]
 
-var b2 = [
+var board1 = [
+[1,2,3,".",".",".",".",".","."],
+[".",".",".",".",".",".",".",".","."],
+[".",".",".",".",".",".",".",".","."],
+[".",".",".",".",".",".",".",".","."],
+[".",".",".",".",".",".",".",".","."],
+[".",".",".",".",".",".",".",".","."],
+[".",".",".",".",".",".",".",".","."],
+[".",".",".",".",".",".",".",".","."],
+[".",".",".",".",".",".",".",".","."]]
+
+var test = [
 [1,2,3,4,5,6,7,8,9],
 [7,8,9,1,2,3,4,5,6],
 [4,5,6,7,8,9,1,2,3],
@@ -23,10 +35,45 @@ var b2 = [
 [9,6,8,2,3,1,5,7,4],
 [5,7,4,9,6,8,2,3,1]]
 
-
+// Game Logic
+var board = board1;
 var methods = new methodsFile(board);
-var methods = new methodsFile(b2);
-
+var userInput;
+console.log("start");
 methods.print();
-console.log(methods.check());
-//console.log(methods.validLine([1,3,6]));
+userInput = input.question("command (type 'help' for helps): ");
+
+//Interactive
+while (userInput != "exit" && methods.check()==false){
+
+  if (userInput == "print"){
+    methods.print(console.log("Complete & Correct: " + methods.check()));
+  }
+  else if (userInput == "status"){
+    if (methods.check()==false){
+      methods.print();
+      console.log("Complete & Correct: " + methods.check());
+     }
+      else break;
+  }
+  else if (userInput == "help"){
+    console.log("ColumnRow-Number: use format to set number to the board")
+    console.log("     Ex: 1a-1 will set column 1, row a to 1")
+    console.log("status : check for completion");
+    console.log("print : print game board");
+    console.log("exit : exit game");
+  }
+  else if (userInput.length == 4 && userInput.charAt(2)=="-"){
+    methods.inputBoard(userInput);
+    methods.print();
+  }
+
+  userInput = input.question("command: ");
+}
+
+if(methods.check()==true){
+  console.log("Complete & Correct: " + methods.check())
+  console.log("Well Played")
+}
+else
+  console.log("Shut Down")
